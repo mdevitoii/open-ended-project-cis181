70e26a15d4ab71bce95454c7186b4fde257b94a4
mdevitoii <mdevitoii04@gmail.com>
2024-11-01 12:55:23 -0400

    stupid error

--- settings.py
+++ settings.py
@@ -1,4 +1,10 @@
 # Michael DeVito II
 # settings.py
-# Additional settings saved sepratelty from archery.py
+# Settings screen for Archery game
+# Also might store future settings (ex: # of rounds, wind enable/disable, etc.)
 
+from graphics import *
+
+def drawScreen(win):
+    settingsText = Text(Point(25,45), "Settings")
+    settingsText.draw(win)
