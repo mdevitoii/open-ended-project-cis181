abe083c8ced58e4dd89d5454f3975558881c23a1
mdevitoii <mdevitoii04@gmail.com>
2024-11-09 12:01:57 -0500

    Settings updates
    added basic settings screen, how to play screen, and actually made use of settings.py

--- archery.py
+++ archery.py
@@ -3,15 +3,15 @@
 # An archery program designed for Theresa
 # Version 1.0
 
-import time, math, os, settings
+import time, math, os
 from graphics import *
+from settings import *
 
 def defineAll(): # Initialize EVERYTHING
-    global openingText, startButton, startText, startCountdownText, timerCountdown, timerSquare, center, timeout, rounds, target, score, currentDirectory, win, arrows
+    global openingText, startButton, startText, settingsButton, settingsText, startCountdownText, timerCountdown, timerSquare, center, timeout, target, score, currentDirectory, win, arrows
     currentDirectory = os.path.dirname(__file__)
     center = Point(250,250)
     score = 0
-    rounds = 3
     target = Image(center, (os.path.join(currentDirectory,"images/target.png")))
     win = GraphWin("Archery Game", 500, 500)
     win.setCoords(0,0,500,500)
@@ -21,8 +21,12 @@ def defineAll(): # Initialize EVERYTHING
     openingText.setSize(30)
     startButton = Rectangle(Point(150,150),Point(350,250))
     startButton.setFill("light green")
-    startText = Text(Point(200, 200), "Click to Start")
+    startText = Text(Point(250, 200), "Click to Start")
     startText.setFill("Black")
+    settingsButton = Rectangle(Point(150,75),Point(350,125))
+    settingsButton.setFill("gray")
+    settingsText = Text(Point(250,100),"Settings/How-To-Play")
+    settingsText.setFill("Black")
     startCountdownText = Text(Point(250,300), "Begin in ... 5")
     startCountdownText.setSize(30)
     startCountdownText.setFill("Black")
@@ -36,7 +40,47 @@ def defineAll(): # Initialize EVERYTHING
     for i in range(1,rounds+1):
         arrows[i] = Image(Point(i * 20,480), (os.path.join(currentDirectory,"images/arrow.png"))) # might need to make arrow.png larger
 
-def clear(win): # clears the window
+def settingsScreen():
+    backButton.draw(win)
+    backText.draw(win)
+    htpButton.draw(win)
+    htpText.draw(win)
+
+    while True:
+        try:
+            mouse = win.getMouse()
+            x = int(mouse.getX())
+            y = int(mouse.getY())
+            if (x >= 150) & (x <= 350):
+                if (y>=75) & (y<=125):
+                    buttonClicked = "GoBack"
+                    break
+                if (y>=400) & (y<=450):
+                    buttonClicked = "HTP"
+                    break
+            
+        except GraphicsError:
+            win.close()
+            print("Window closed prematurely.")
+            break
+    
+    try:
+        if buttonClicked == "GoBack":
+            win.close()
+            main()
+        if buttonClicked == "HTP":
+            clear()
+            htpScreen()
+
+
+    except:
+        print("Something went wrong! Exit code: GOBACK win.close")
+
+def htpScreen():
+    win.setBackground("light yellow")
+    
+
+def clear(): # clears the window
     for item in win.items[:]:
         item.undraw()
 
@@ -137,6 +181,9 @@ def main(): # main method
     openingText.draw(win)
     startButton.draw(win)
     startText.draw(win)
+    settingsButton.draw(win)
+    settingsText.draw(win)
+    buttonClicked = ""
     
     # checks what option the user chooses and sends them to desired option
     while True:
@@ -146,26 +193,37 @@ def main(): # main method
             y = int(mouse.getY())
             if (x >= 150) & (x <= 350):
                 if (y >= 150) & (y <= 250):
+                    buttonClicked = "Start"
                     break
+                elif (y>=75) & (y<=125):
+                    buttonClicked = "Settings"
+                    break
+            
         except GraphicsError:
             win.close()
             print("Window closed prematurely.")
             break
 
     try:
-        win.setBackground("light green")
-        clear(win)
-        countToStart()
-        timerSquare.draw(win)
-        timerCountdown.draw(win)
-        target.draw(win)
-        for i in arrows:
-            arrows[i].draw(win)
-        for i in range(1,rounds+1):
-            game()
-            arrows[i].undraw() 
-        print(f"Your final score is: {score}\nClick to Quit.")
-        clear(win)
+        if buttonClicked == "Start":
+            win.setBackground("light green")
+            clear()
+            countToStart()
+            timerSquare.draw(win)
+            timerCountdown.draw(win)
+            target.draw(win)
+            for i in arrows:
+                arrows[i].draw(win)
+            for i in range(1,rounds+1):
+                game()
+                arrows[i].undraw() 
+            print(f"Your final score is: {score}\nClick to Quit.")
+            clear()
+        elif buttonClicked == "Settings":
+            win.setBackground("light gray")
+            clear()
+            settingsScreen()
+
     except:
         print("Something went wrong. Please try again!")
       
@@ -191,7 +249,7 @@ except GraphicsError:
 # Version 2 
     # timer DONE
     # 5 shots DONE
-    # Show a visual of how many shots left
+    # Show a visual of how many shots left DONE
 # Version 3
     # music?
     # wind factor
--- settings.py
+++ settings.py
@@ -3,8 +3,23 @@
 # Settings screen for Archery game
 # Also might store future settings (ex: # of rounds, wind enable/disable, etc.)
 
-from graphics import *
+from graphics import*
 
-def drawScreen(win):
-    settingsText = Text(Point(25,45), "Settings")
-    settingsText.draw(win)
+rounds = 3
+howToPlay = (
+    f"Welcome to Archery! A game created by Michael DeVito\n"
+    f"Dedicated to Theresa I.\n\n"
+    f"Archery can be played by either using the arrow keys or the Mouse. You can configure which input you use via the settings screen.\n"
+    f"The goal is to shoot as close to the center ring as you can. The closer you are, the higher the point value!\n"
+    f"The game defaults to 3 shots per round with 5 seconds to shoot each shot. These values can be configured via the settings screen.\n"
+    f"Gameplay too boring? Try out the difficulties and different gamemodes in the settings screen!\n"
+    f"Thank you for playing, and have fun!"
+)
+
+backButton = Rectangle(Point(150,75),Point(350,125))
+backButton.setFill("gray")
+backText = Text(Point(250,100),"Save and Exit")
+backText.setFill("Black")
+htpButton = Rectangle(Point(150,400),Point(350,450))
+htpButton.setFill("Light green")
+htpText = Text(Point(250,425), "How to Play")
